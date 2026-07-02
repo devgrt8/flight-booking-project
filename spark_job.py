1626afd513b8eda333662a28c6fca3f357a3a5b0
Dev <devgrt8@gmail.com>
2026-07-02 14:56:36 +0530

    added spark_job.py file

---
+++ spark_job.py
@@ -0,0 +1,105 @@
+import argparse, logging, os, sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when, count, avg, round, lit, expr
+
+
+# Initialize Logging
+
+logging.basicConfig \
+            (level=logging.INFO, \
+            format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+def main(env, gcs_bucket, bq_project, bq_dataset, transformed_table, route_insights_table, origin_insights_table):
+    # Initialize Spark Session
+    try:
+        spark = SparkSession.builder \
+            .appName("Flight Booking Analysis") \
+            .config("spark.sql.catalogImplementation", "hive") \
+            .getOrCreate()
+        logger.info("Spark Session initialized successfully.")
+
+        # Read input data from GCS
+        input_path = f"gs://{gcs_bucket}/source_{env}/*.csv"
+        logger.info(f"Reading input data from: {input_path}")
+
+        # Read Data from GCS
+        data = spark.read.csv(input_path, header=True, inferSchema=True)
+        logger.info(f"Data read successfully with {data.count()} records.")
+
+        # Data Transformation
+        transformed_data = data.withColumn("is_weeekkend", when(col("day_of_week").isin(["Saturday", "Sunday"]), lit(1)).otherwise(lit(0))) \
+                                .withColumn("lead_time_category", when(col("purchase_lead") < 7, lit("last-minute") ) \
+                                                                    .when((col("purchase_lead") >= 7) & (col("purchase_lead") <= 30), lit("short-term")) \
+                                                                    .otherwise(lit("long-term"))) \
+                                .withColumn("booking_success_rate", expr('booking_completed / num_passengers'))
+        
+        logger.info("Data transformation completed successfully.")
+
+        #Aggregate Data for Route Insights
+        route_insights = transformed_data.groupBy("route") \
+                            .agg(count("*").alias("total_bookings"), \
+                            avg("flight_duration").alias("avg_flight_duration"), \
+                            avg("length_of_stay").alias("avg_length_of_stay"), 
+                            )
+        
+        logger.info("Route insights aggregation completed successfully.")
+
+        #Aggregate Data for Origin Insights
+        origin_insights = transformed_data.groupBy("booking_origin") \
+                            .agg(count("*").alias("total_bookings"), \
+                            avg("booking_success_rate").alias("avg_booking_success_rate"), \
+                            avg("purchase_lead").alias("avg_purchase_lead"), )
+        
+        logger.info("Origin insights aggregation completed successfully.")
+
+        # Write Transformed Data to BigQuery
+        # data will be written directly to BigQuery as data is small enough to fit in memory 
+        transformed_data.write.format("bigquery") \
+            .option("table", f"{bq_project}.{bq_dataset}.{transformed_table}") \
+            .option("writeMethod", "direct") \
+            .mode("overwrite") \
+            .save()
+
+        logger.info(f"Transformed data written to BigQuery table: {bq_project}.{bq_dataset}.{transformed_table}")
+
+        # Write Route Insights to BigQuery
+        route_insights.write.format("bigquery") \
+            .option("table", f"{bq_project}.{bq_dataset}.{route_insights_table}") \
+            .option("writeMethod", "direct") \
+            .mode("overwrite") \
+            .save()
+
+        logger.info(f"Route insights written to BigQuery table: {bq_project}.{bq_dataset}.{route_insights_table}")
+
+        # Write Origin Insights to BigQuery
+        origin_insights.write.format("bigquery") \
+            .option("table", f"{bq_project}.{bq_dataset}.{origin_insights_table}") \
+            .option("writeMethod", "direct") \
+            .mode("overwrite") \
+            .save()
+        
+        logger.info(f"Origin insights written to BigQuery table: {bq_project}.{bq_dataset}.{origin_insights_table}")
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+
+    finally:
+        # Stop Spark Session
+        spark.stop()
+        logger.info("Spark Session stopped.")
+
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Flight Booking Analysis Spark Job")
+        parser.add_argument("--env", required=True, help="Environment (e.g., dev, prod)")
+        parser.add_argument("--gcs_bucket", required=True, help="GCS bucket name")
+        parser.add_argument("--bq_project", required=True, help="BigQuery project ID")
+        parser.add_argument("--bq_dataset", required=True, help="BigQuery dataset name")
+        parser.add_argument("--transformed_table", required=True, help="BigQuery table for transformed data")
+        parser.add_argument("--route_insights_table", required=True, help="BigQuery table for route insights")
+        parser.add_argument("--origin_insights_table", required=True, help="BigQuery table for origin insights")
+        args = parser.parse_args()
+
+        main(args.env, args.gcs_bucket, args.bq_project, args.bq_dataset, args.transformed_table, args.route_insights_table, args.origin_insights_table)
